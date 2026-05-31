67ef47dcd0e72eccfd7d50947d26adc940cedb0e
Rustam <nusratov_r@mail.ru>
2026-05-31 20:31:41 +0300

    started writing SSH report script

---
+++ ssh_report.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+BLUE='\033[0;34m'
+CYAN='\033[0;36m'
+BOLD='\033[1m'
+RESET='\033[0m'
+
+clear
+
+clear
+
+echo -e "${BOLD}${CYAN}"
+echo "========================================"
+echo "  SSH ОТЧЁТ"
+echo "========================================"
+echo -e "${RESET}"
+
+echo -e "${BOLD}____________________________________${RESET}"
+echo -e "\n${BOLD}${GREEN}  УСПЕШНЫЕ ВХОДЫ${RESET}"
+echo -e "${BOLD}____________________________________${RESET}"
+echo ""
+journalctl | grep -i sshd | grep -i accepted | tail -10
