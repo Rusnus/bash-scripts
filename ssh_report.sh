242026e543fe41ac2dccb868032564243fbf18d8
Rustam <nusratov_r@mail.ru>
2026-06-04 21:45:52 +0300

    added users list

--- ssh_report.sh
+++ ssh_report.sh
@@ -18,3 +18,7 @@ ss -tlnp | grep LISTEN | awk '{print $4, $6}'
 echo""
 echo "Заблокированные IP (fail2ban)"
 fail2ban-client status sshd | grep "Banned IP list" -A 10
+
+echo""
+echo "Список пользователей"
+getent passwd | awk -F: '{print $1}'
