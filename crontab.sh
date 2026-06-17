d9edeb14a80e8ff370a61a620706e587e0de51d4
Rustam <nusratov_r@mail.ru>
2026-06-17 22:38:23 +0300

    added steps 1-4 to the script on 17.06

--- crontab.sh
+++ crontab.sh
@@ -9,6 +9,8 @@ fi
 
 clear
 echo "CRON"
+
+#Текущие задачи
 echo -e "\nТекущие задачи:"
 CURRENT_CRON=$(crontab -l 2>/dev/null)
 if [ -z "$CURRENT_CRON" ]; then
@@ -17,8 +19,39 @@ else
         echo "$CURRENT_CRON"
 fi
 
+#Добавление задачи
 echo ""
 read -p "Добавить новую задачу? (y/n): " ADD_JOB
 if [ "$ADD_JOB" != "y" ] && [ "$ADD_JOB" != "Y" ]; then
         exit 0
 fi
+
+#Путь к скрипту
+echo ""
+while true; do
+	read -e -r -p "Введите полный путь к скрипту:" SCRIPT_PATH
+
+	if [ -f "$SCRIPT_PATH" ]; then
+		break
+	else 
+		echo "Файл не найден."
+		echo ""
+	fi
+done
+
+#Расписание задачи
+echo ""
+while true; do
+	read -r -p "Введите рассписание (пример: 0 9 * * *):" CRON_TIME
+	FIELDS_COUNT=$(echo "$CRON_TIME" | awk '{print NF}')
+
+	if [ "$FIELDS_COUNT" -ne 5 ]; then
+		echo "Ошибка: расписание должно состоять из 5-ти элементов(минута час день месяц день_недели)"
+		echo ""
+	elif [[ "$CRON_TIME" =~ [^0-9\ \*\,/\-] ]]; then
+		echo "Ошибка: расписание не должно содержать буквы и не допустимые знаки."
+		echo ""
+	else
+		break
+	fi
+done
